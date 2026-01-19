6c35c242e02f30935fa9319dc4a6b94576f82424
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2026-01-19 10:09:40 +0530

    created game

--- Game.py
+++ Game.py
@@ -1,2 +1,24 @@
-print("game start")
-print("Hello")
+
+
+auth = []
+name = input("Enter your name")
+coins = int(input("Enter your coins"))
+level = int(input("Enter your level"))
+db = []
+auth.append(authen(name,coins,level,db))
+
+failure_count = 0
+for i in range (0,3):
+    score = Scoreget()
+    if score>=60:
+        level+=1
+        coins+=100
+        print("you passed this level")
+        break
+    else:
+        failure_count+=1
+
+if failure_count ==3:
+    level-=1
+    coins-=100
+    print("better luck next time")
